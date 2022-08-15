0b7d98281ea2b7726c98c559080c801bad70310a
waqaskareempk <91574930+waqaskareempk@users.noreply.github.com>
2022-08-15 05:04:16 +0300

    Added dice challenge

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+
+var randomDiceImg = "dice" + randomNumber1 + ".png";
+
+var randomImgSrc = "images/" + randomDiceImg;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImgSrc)
+
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+
+var randomImageSrc2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2)
+
+// player one wins
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} 
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} 
+else {
+    document.querySelector("h1").innerHTML = "Draw!";
+}
